4cfa468eeae57793d375145e8f7ac0698d1ee184
lorevis92 <lorenzo.visconti@hotmail.it>
2023-08-25 11:14:01 +0200

    Creazione progeto Sring Boot Maven con Lombock installato

---
+++ src/main/java/Epicode/ProgettoSettimana07/ProgettoSettimana07Application.java
@@ -0,0 +1,13 @@
+package Epicode.ProgettoSettimana07;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgettoSettimana07Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgettoSettimana07Application.class, args);
+	}
+
+}
